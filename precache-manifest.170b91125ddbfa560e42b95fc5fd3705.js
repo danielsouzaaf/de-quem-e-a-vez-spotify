a215d2493f1f943dc543b4870cc3af818da8055b
Daniel Souza Affonso Ferreira <daniel.souza.af@gmail.com>
2019-01-09 13:17:18 -0300

    Updates

---
+++ precache-manifest.170b91125ddbfa560e42b95fc5fd3705.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "da181273d1873010fea574371c7b4a78",
+    "url": "/de-quem-e-a-vez-spotify/static/media/mvb.da181273.jpg"
+  },
+  {
+    "revision": "32b3ee0faf54958b75f1",
+    "url": "/de-quem-e-a-vez-spotify/static/css/main.0b3e4b75.chunk.css"
+  },
+  {
+    "revision": "12515d38c4eb5e955f9c",
+    "url": "/de-quem-e-a-vez-spotify/static/js/1.12515d38.chunk.js"
+  },
+  {
+    "revision": "0efad879dca1918ec909",
+    "url": "/de-quem-e-a-vez-spotify/static/js/runtime~main.0efad879.js"
+  },
+  {
+    "revision": "be575f5d50cec662c2d03117f1d48c19",
+    "url": "/de-quem-e-a-vez-spotify/static/media/dsa.be575f5d.jpg"
+  },
+  {
+    "revision": "32b3ee0faf54958b75f1",
+    "url": "/de-quem-e-a-vez-spotify/static/js/main.32b3ee0f.chunk.js"
+  },
+  {
+    "revision": "30334d8594fd39f3c7bacb823638ea4a",
+    "url": "/de-quem-e-a-vez-spotify/static/media/aks.30334d85.jpg"
+  },
+  {
+    "revision": "187434aa942d29ff89ae34baa50bd1c5",
+    "url": "/de-quem-e-a-vez-spotify/static/media/lfm.187434aa.jpg"
+  },
+  {
+    "revision": "bf6567516d0f9fa9262b84e562f5ddb7",
+    "url": "/de-quem-e-a-vez-spotify/static/media/meg.bf656751.jpg"
+  },
+  {
+    "revision": "7c99f23e8d4ba0243469b9b86acb1b00",
+    "url": "/de-quem-e-a-vez-spotify/static/media/mlm.7c99f23e.jpg"
+  },
+  {
+    "revision": "89e96679ac9a16c8ae214f02e9ff9371",
+    "url": "/de-quem-e-a-vez-spotify/index.html"
+  }
+];
